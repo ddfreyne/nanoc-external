b99964831ba4933b575e47c922d6f966b0c52475
Denis Defreyne <denis.defreyne@stoneship.org>
2014-02-20 22:51:58 +0100

    Remove base64-based test
    It is not cross-platform. A single test should be enough.

--- test/filters/test_external.rb
+++ test/filters/test_external.rb
@@ -4,19 +4,13 @@ require 'helper'
 
 class Nanoc::External::FilterTest < Minitest::Test
 
-  def test_filter_no_options
-    filter = ::Nanoc::External::Filter.new({})
-    src = "Shall I compare thee to a Summer's day?"
-    out = 'U2hhbGwgSSBjb21wYXJlIHRoZWUgdG8gYSBTdW1tZXIncyBkYXk/Cg=='
-    assert_equal(out, filter.run(src, :exec => '/usr/bin/base64'))
-  end
-
-  def test_filter_with_options
+  def test_filter
     filter = ::Nanoc::External::Filter.new({})
     src = <<-SHAKESPEARE
     Shall I compare thee to a Summer's day?
     Thou art more lovely and more temperate
     SHAKESPEARE
-    assert_match(/^\s*2\s*$/, filter.run(src, :exec => 'wc', :options => %w( -l ) ))  
+    assert_match(/^\s*2\s*$/, filter.run(src, :exec => 'wc', :options => %w( -l ) ))
   end
+
 end
